f6018b49832e274175cc781a5b7b04e79f56504a
Ricardo Pareja <pareja.r@gmail.com>
2019-01-09 11:20:56 -0200

    Updates

---
+++ precache-manifest.cb602158cca7518ee515e6f76ec4a541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/udacity-maps/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "e988c55cd9fc750274c0",
+    "url": "/udacity-maps/static/js/runtime~main.e988c55c.js"
+  },
+  {
+    "revision": "e7ebb66203ece5974d2a",
+    "url": "/udacity-maps/static/js/main.e7ebb662.chunk.js"
+  },
+  {
+    "revision": "1072399bf7ab154c4e8f",
+    "url": "/udacity-maps/static/js/1.1072399b.chunk.js"
+  },
+  {
+    "revision": "e7ebb66203ece5974d2a",
+    "url": "/udacity-maps/static/css/main.46344ef6.chunk.css"
+  },
+  {
+    "revision": "1072399bf7ab154c4e8f",
+    "url": "/udacity-maps/static/css/1.274b2170.chunk.css"
+  },
+  {
+    "revision": "355d44b2a4ddd682d30d8cd444bd6e52",
+    "url": "/udacity-maps/index.html"
+  }
+];
