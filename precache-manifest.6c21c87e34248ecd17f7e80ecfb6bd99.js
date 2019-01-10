8cfcc93f8b0ee1af3ce704db9caea97c8a1fd97d
Ricardo Pareja <pareja.r@gmail.com>
2019-01-10 15:21:46 -0200

    Updates

---
+++ precache-manifest.6c21c87e34248ecd17f7e80ecfb6bd99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e988c55cd9fc750274c0",
+    "url": "/udacity-maps/static/js/runtime~main.e988c55c.js"
+  },
+  {
+    "revision": "cd0b0e1a7b12d1b274ff",
+    "url": "/udacity-maps/static/js/main.cd0b0e1a.chunk.js"
+  },
+  {
+    "revision": "87edd19c41963d6c81e6",
+    "url": "/udacity-maps/static/js/1.87edd19c.chunk.js"
+  },
+  {
+    "revision": "cd0b0e1a7b12d1b274ff",
+    "url": "/udacity-maps/static/css/main.a6a645cd.chunk.css"
+  },
+  {
+    "revision": "87edd19c41963d6c81e6",
+    "url": "/udacity-maps/static/css/1.274b2170.chunk.css"
+  },
+  {
+    "revision": "3bd9da3a042c6c9898445b3facb109b1",
+    "url": "/udacity-maps/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3b5944ad81acf0b0c756d8fab73d511.js"
+  "/udacity-maps/precache-manifest.6c21c87e34248ecd17f7e80ecfb6bd99.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/udacity-maps/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
